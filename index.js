a991677742401844f7f3ad4b4837bb3edb138d6c
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 20:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(array, callback){
-  return array.forEach(callback)
-}
-
-// Add your changeCompletely() function here:
-function changeCompletely(array){
-array.forEach(changeCompletely)
-}
