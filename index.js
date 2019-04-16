498854442949f5890eea2f9573a84b2203d9c649
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 20:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
